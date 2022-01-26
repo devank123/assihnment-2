5474b96308c73837821245c6d3fb127392ddc69a
devank123 <97097960+devank123@users.noreply.github.com>
2022-01-26 15:15:01 +0530

    Add files via upload

---
+++ asgn2-result.py
@@ -0,0 +1,39 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/devan/Desktop/python/asgn-2.py ===============
+Answer-1
+Ans 1.a--> Length of the input string is: 35
+Ans 1.b--> Reverse of input string is: egaugnal evitisnes esac a si nohtyP
+Ans 1.c--> Sliced string is: a case sensitive
+Ans 1.d--> String after replacing is: Python is object oriented language
+Ans 1.e--> Index of substring "a" is: 10
+Ans 1.f--> String after removing " " is: Pythonisacasesensitivelanguage
+
+Answer-2
+Hey, Devank Garg Here!
+My SID is 21104098
+I am from EE department and my CGPA is 9.900000
+
+Answer-3
+3.a--> a&b= 8
+3.b--> a|b= 58
+3.c--> a^b= 50
+3.d--> a<<2= 224 and b<<2= 40
+3.e--> a>>2= 14 and b>>4= 0
+
+Answer-4
+Enter first number:5
+Enter second number:4
+Enter third number:6
+greatest of three numbers is 6
+
+Answer-5
+Enter a string:my name is devank
+Yes
+
+Answer-6
+Enter side a length:7
+Enter side b length:8
+Enter side c length:9
+Yes
